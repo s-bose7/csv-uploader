e3711828bcd2f21f7e0cbf78cdb39c40813795ad
Sudip Bose <sbose007ime.work@gmail.com>
2024-05-09 12:05:32 +0530

    added csv utilities validators are not implemented

--- utils/csv_utils.py
+++ utils/csv_utils.py
@@ -1,3 +1,29 @@
 # This module should contain functions for reading, parsing, and transforming CSV files 
 # before uploading the data to the database.
 
+import pandas as pd
+from pandas import DataFrame
+
+from utils.validators import Validator
+
+
+def read_file(file_path: str, file_type="csv")->DataFrame:
+    try:
+        if file_type == "csv":
+            df = pd.read_csv(file_path)
+        else:
+            df = pd.read_excel(file_path)
+
+    except FileNotFoundError:
+        print(f"Error: File '{file_path}' not found.")
+        return None
+    except Exception as e:
+        print(f"An error occurred while reading the file: {e}")
+        return None
+    
+    return df
+
+
+def validate_data(raw_input: DataFrame)->DataFrame:
+    return raw_input
+    
--- utils/validators.py
+++ utils/validators.py
@@ -1,2 +1,7 @@
 # This module should contain functions for validating and normalizing the data read from CSV 
 # files.
+
+class Validator:
+
+    def __init__(self) -> None:
+        pass
