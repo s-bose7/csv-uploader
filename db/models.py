ea752d25fc0bdd91e9f376b0032ebf6a87653e61
Sudip Bose <sbose007ime.work@gmail.com>
2024-05-09 12:03:28 +0530

    pluralized model names

--- db/models.py
+++ db/models.py
@@ -26,7 +26,7 @@ class Segments(Base):
         return f"Segments(id={self.id}, name='{self.name}', created_at='{self.created_at}')"
 
 
-class Organization(Base):
+class Organizations(Base):
     __tablename__ = 'organizations'
 
     id = Column(Integer, primary_key=True)
@@ -51,13 +51,13 @@ class Organization(Base):
 
     # A one-to-many relationship between the Organization and Segment models, 
     # where one Segment can have multiple Organization instances.
-    segment = relationship('Segment', backref='organizations')
+    segment = relationship('Segments', backref='organizations')
 
     def __repr__(self):
         return f"Organization(id={self.id}, name='{self.name}', segment_id={self.segment_id})"
     
 
-class Club(Base):
+class Clubs(Base):
     __tablename__ = 'clubs'
 
     id = Column(Integer, primary_key=True)
@@ -68,13 +68,13 @@ class Club(Base):
 
     # A one-to-many relationship between the Club and Organization models, 
     # where one Organization can have multiple Club instances.
-    organization = relationship('Organization', backref='clubs')
+    organization = relationship('Organizations', backref='clubs')
 
     def __repr__(self):
         return f"Club(id={self.id}, name='{self.name}', organization_id={self.organization_id})"
 
 
-class Agent(Base):
+class Agents(Base):
     __tablename__ = 'agents'
 
     id = Column(Integer, primary_key=True)
@@ -87,19 +87,19 @@ class Agent(Base):
 
     # A one-to-many relationship between the Agent and Club models, 
     # where one Club can have multiple Agent instances. 
-    club = relationship('Club', backref='agents')
+    club = relationship('Clubs', backref='agents')
     # A one-to-one relationship between the Agent and Contact models, 
     # where one Contact can have at most one Agent instance.
-    contact = relationship('Contact', backref='agent', uselist=False)
+    contact = relationship('Contacts', backref='agents', uselist=False)
     # A one-to-many relationship between the Agent and Organization models, 
     # where one Organization can have multiple Agent instances.
-    organization = relationship('Organization', backref='agents')
+    organization = relationship('Organizations', backref='agents')
 
     def __repr__(self):
         return f"Agent(id={self.id}, rank={self.rank}, club_id={self.club_id}, contact_id={self.contact_id}, organization_id={self.organization_id})"
     
 
-class Contact(Base):
+class Contacts(Base):
     __tablename__ = 'contacts'
 
     id = Column(Integer, primary_key=True)
@@ -115,13 +115,13 @@ class Contact(Base):
 
     # A one-to-many relationship between the Contact and Organization models,
     # where one Organization can have multiple Contact instances.
-    organization = relationship('Organization', backref='contacts')
+    organization = relationship('Organizations', backref='contacts')
     # A one-to-many relationship between the Contact and Club models,
     # where one Club can have multiple Contact instances.
-    club = relationship('Club', backref='contacts')
+    club = relationship('Clubs', backref='contacts')
     # A one-to-one relationship between the Contact and Agent models,
     # where one agent can have at most one Contact instance. 
-    agent = relationship('Agent', backref='contact', uselist=False)
+    agent = relationship('Agents', backref='contacts', uselist=False)
 
     def __repr__(self):
         return f"Contact(id={self.id}, email='{self.email}', first_name='{self.first_name}', position='{self.position}')"
