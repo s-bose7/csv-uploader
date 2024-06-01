0cb73ecc726d4414d35eb227f516ba413d23d1ad
Sudip Bose <sbose007ime.work@gmail.com>
2024-06-02 02:40:40 +0530

    Update contact uploading process if an existing email encounters with a new organization, then old organization values would get replaced by this new organization values

--- main.py
+++ main.py
@@ -1,22 +1,17 @@
 # Orchestrate the data migration process.
 import sys
 import logging
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-from db.models import (
-    Segments,
-    Organizations,
-    Clubs,
-    Contacts,
-    Agents
-)
-
 from config import db_config
 from utils.csv_utils import read_file, validate_data
+from db.db_utils import generate_slug
+from db.models import Segments, Organizations, Clubs, Contacts, Agents
 
 # read raw input
-FILE_PATH = "data/csv_files/test.csv"
+FILE_PATH = "data/csv_files/test_new_agent_rank_logic.csv"
 data = read_file(file_path=FILE_PATH)
 if data.empty:
     sys.exit(1)
@@ -44,7 +39,7 @@ session = Session()
 # insert data
 for index, row in validated_data.iterrows():
     
-    # filter segment by name if it exist
+    # Filter segment by name if it exist
     segment = session.query(Segments).filter_by(name=row['organization_category']).first()
     if segment is None:
         # create a segment instance
@@ -53,17 +48,17 @@ for index, row in validated_data.iterrows():
         session.commit()
         print(segment.__repr__)
 
-    # secondary identifier for organization
-    slug = str(row['organization_name'] + str(row['address'])).replace(" ", "").lower()
+    # Secondary identifier for organization
+    slug = generate_slug(row['organization_name'], row['address'])
     
-    # filter organization by slug and segment.id if it exist
+    # Filter organization by slug and segment.id if it exist
     organization = session.query(Organizations).filter_by(
         slug=slug, 
         segment_id=segment.id
     ).first()
     
     if organization is None:
-        # create an organization instance
+        # Create an organization instance
         organization = Organizations (
             name=row['organization_name'],
             segment_id=segment.id,
@@ -89,37 +84,58 @@ for index, row in validated_data.iterrows():
 
     club: Clubs = None
     if isinstance(row["club_name"], str) and row["club_name"] != "":
-        # filter club by club name and organization.id if it exist
+        # Filter club by club name and organization.id if it exist
         club = session.query(Clubs).filter_by(
             name=row['club_name'], 
             organization_id=organization.id
         ).first()
         
         if club is None:
-            # create a club instance
+            # Create a club instance
             club = Clubs(name=row['club_name'], organization_id=organization.id)
             session.add(club)
             session.commit()
             print(club.__repr__)    
 
+    # Filter contact by email
+    contact = session.query(Contacts).filter_by(email=row['contact_email']).first()
 
-    # filter contact by email and organization.id if it exist
+    if contact and contact.organization_id != organization.id:
+        contact.organization.name = organization.name
+        contact.organization.street_address = organization.street_address
+        contact.organization.city = organization.city
+        contact.organization.state = organization.state
+        contact.organization.zip = organization.zip
+        contact.organization.slug = generate_slug(organization.name, organization.street_address)
+        contact.first_name = row["contact_name"]
+        contact.source = row["contact_source"]
+        contact.position = row["contact_position"]
+        if club is not None:
+            contact.club_id = club.id
+        session.commit()
+        
+        # delete this organization
+        session.delete(organization)
+        session.commit()
+        continue
+    
+    # Filter contact by email and organization.id if it exist
     contact = session.query(Contacts).filter_by(
         email=row['contact_email'],
         organization_id=organization.id
     ).first()
 
     if contact is None:
-        # create a contact instance
+        # Create a contact instance
         contact = Contacts (
             email=row['contact_email'],
             organization_id=organization.id,
-            # set other contact fields
+            # Set other contact fields
             first_name = row["contact_name"],
             source=row["contact_source"],
             position=row["contact_position"]
         )
-        # iff club exist for this contact
+        # Iff club exist for this contact
         if club is not None: 
             contact.club_id = club.id
 
@@ -127,20 +143,20 @@ for index, row in validated_data.iterrows():
         session.commit()
         print(contact.__repr__)
 
-    # filter agent by contact.id and organization.id if it exist
+    # Filter agent by contact.id and organization.id if it exist
     agent = session.query(Agents).filter_by(
         contact_id=contact.id,
         organization_id=organization.id
     ).first()
 
     if agent is None:
-        # create an agent instance
+        # Create an agent instance
         agent = Agents (
             rank=row['agent_rank'],
             contact_id=contact.id,
             organization_id=organization.id
         )
-        # iff club exist for this agent
+        # Iff club exist for this agent
         if club is not None: 
             agent.club_id = club.id
 
@@ -149,7 +165,7 @@ for index, row in validated_data.iterrows():
         print(agent.__repr__)
 
 
-# close connection
+# Close connection
 session.close()
 engine.dispose()
 sys.exit(0)
