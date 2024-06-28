3f8596a36d83503bb8dfb3334c54f9f9e7d25ca3
Sudip Bose <sbose007ime.work@gmail.com>
2024-06-28 15:34:05 +0530

    Update: Agents will be autogenerated upon inserting in contacts Used PL/SQL function and trigger to autogenerate agents upon inserting new contacts in contacts table. An agent rank will get assigned automatically to a new agent for a given organization.

--- main.py
+++ main.py
@@ -9,12 +9,11 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 from db.db_utils import generate_slug
-from db.db_utils import update_agent_ranks
-from db.db_utils import is_most_recent_contact
+from db.db_utils import add_and_commit
 from db.db_utils import remove_outdated_emails_from_agents
 
+from db.models import Segments, Organizations, Clubs, Contacts
 from utils.csv_utils import read_file, validate_data, sanitize_json
-from db.models import Segments, Organizations, Clubs, Contacts, Agents
 
 
 # Read raw input
@@ -55,9 +54,7 @@ for index, row in validated_data.iterrows():
     if segment is None:
         # create a segment instance
         segment = Segments(name=row['segment_name'])
-        session.add(segment)
-        session.commit()
-        print(segment.__repr__)
+        add_and_commit(session, segment)
 
     # Process organization
     # Secondary identifier for organization
@@ -97,9 +94,7 @@ for index, row in validated_data.iterrows():
         
         organization.custom_fields = sanitize_json(organization.custom_fields)
 
-        session.add(organization)
-        session.commit()
-        print(organization.__repr__)
+        add_and_commit(session, organization)
 
     # Process clubs
     club: Clubs = None
@@ -113,9 +108,7 @@ for index, row in validated_data.iterrows():
         if club is None:
             # Create a club instance
             club = Clubs(name=row['club_name'], organization_id=organization.id)
-            session.add(club)
-            session.commit()
-            print(club.__repr__)    
+            add_and_commit(session, club)   
         
     # Process contacts
     # Filter contact by email and organization.id if it exist
@@ -140,39 +133,15 @@ for index, row in validated_data.iterrows():
                 contact.custom_fields["source"] = row["contact_source"]
             if row["contact_position"]:
                 contact.custom_fields["position"] = row["contact_position"]
-            
+
             contact.custom_fields = sanitize_json(contact.custom_fields)
 
             # If club exist for this contact
             if club is not None: 
                 contact.club_id = club.id
-            session.add(contact)
-            session.commit()
-            print(contact.__repr__)
-
-    # Process agent
-    agent: Agents = None
-    # Filter agent by contact.id and organization.id if it exist
-    if contact is not None:
-        agent = session.query(Agents).filter_by(
-            contact_id=contact.id, organization_id=organization.id
-        ).first()
-
-    if agent is None and is_most_recent_contact(session, organization, contact):
-        update_agent_ranks(session, organization)
-        # Create an agent instance
-        agent = Agents (
-            rank = 1, 
-            contact_id=contact.id,
-            organization_id=organization.id
-        )
-        # If club exist for this agent
-        if club is not None: 
-            agent.club_id = club.id
-        session.add(agent)
-        session.commit()
-        print(agent.__repr__)
-
+            
+            add_and_commit(session, contact)
+            
 
 remove_outdated_emails_from_agents(session)
 
@@ -180,3 +149,7 @@ remove_outdated_emails_from_agents(session)
 session.close()
 engine.dispose()
 sys.exit(0)
+
+
+# Test cases
+# Bulk upload
