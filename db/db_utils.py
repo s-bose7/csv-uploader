756030e95a2874d51c67f0c3da189bfdd46e02b6
Sudip Bose <sbose007ime.work@gmail.com>
2024-06-28 15:26:24 +0530

    Add: function to make database commit

--- db/db_utils.py
+++ db/db_utils.py
@@ -1,10 +1,20 @@
 import re
 
-from sqlalchemy import desc
 from sqlalchemy.orm import Session
+from sqlalchemy.exc import SQLAlchemyError
+
+from db.models import Agents
 from datetime import datetime, timedelta
 
-from db.models import Agents, Organizations, Contacts
+
+def add_and_commit(session: Session, new_record)->None:
+    try:
+        session.add(new_record)
+        session.commit()
+        print(new_record.__repr__())
+    except SQLAlchemyError as e:
+        session.rollback()
+        print(f"An error occurred: {e}")
 
 
 def generate_slug(organization_name:str, address: str)->str:
@@ -28,26 +38,5 @@ def remove_outdated_emails_from_agents(session: Session):
         session.query(Agents).filter(Agents.id == agent.id).update({"contact_id": None})
     # Delete the outdated agents
     session.query(Agents).filter(Agents.created_at <= two_years_ago).delete(synchronize_session=False)
-
-    session.commit()
-
-
-def is_most_recent_contact(session: Session, organization: Organizations, contact: Contacts)->bool:
-    if contact is None:
-        return False
-    # Query the most recent contact for the given organization
-    most_recent_contact = session.query(Contacts).filter_by(
-        organization_id=organization.id
-    ).order_by(desc(Contacts.created_at)).first()
-    # Check if the given contact_id is the most recent one
-    return most_recent_contact is not None and most_recent_contact.id == contact.id
-
-
-def update_agent_ranks(session: Session, organization: Organizations)->None:
-    # Query all agents associated with the organization
-    agents = session.query(Agents).filter_by(organization_id=organization.id).all()
-    # Increment the rank of each agent by 1
-    for agent in agents:
-        agent.rank += 1
 
     session.commit()
