8341b8a634318ac9919203f0898565537470fef6
Sudip Bose <sbose007ime.work@gmail.com>
2024-11-17 11:34:25 +0530

    Add: Raw organizations script

---
+++ raw_org_uploads.py
@@ -0,0 +1,58 @@
+import sys
+import logging
+
+from datetime import datetime
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from config import db_config
+from utils.csv_utils import read_file
+from db.models import Organizations
+
+from db.db_utils import generate_slug, add_and_commit
+
+
+FILE_PATH = "data/csv_files/raw_orgs - Sheet1.csv"
+data = read_file(file_path=FILE_PATH)
+
+try:
+    ENV = sys.argv[1] if len(sys.argv) > 1 else "-development"
+    DATABASE_URL = db_config.get_database_url(environment=ENV)
+    # Connect to database engine
+    engine = create_engine(DATABASE_URL)
+except Exception as e:
+    logging.error(e)
+    sys.exit(1)
+
+
+Session = sessionmaker(bind=engine)
+session = Session()
+
+for index, row in data.iterrows():
+
+    # Process organization
+    # Secondary identifier for organization
+    slug = generate_slug(row['organization_name'], row['organization_address'])
+    # Filter organization by slug and segment.id if it exist
+    organization = session.query(Organizations).filter_by(slug=slug).first()
+    
+    if organization is None:
+        # Create an organization instance
+        organization = Organizations (
+            name=row['organization_name'],
+            slug=slug,
+            street_address=row['organization_address'],
+            city=row['organization_city'],
+            state=row['organization_state'],
+            zip=row['organization_zip'],
+            custom_fields = {},
+        )
+        organization.created_at = datetime.now()
+        if row["irs_ein"] and row["irs_ntee_code"]:
+            organization.custom_fields["irs_ein"] =  row["irs_ein"]
+            organization.custom_fields["irs_ntee_code"] = row["irs_ntee_code"]
+
+        add_and_commit(session, organization)
+    
+print("\nProcess finished with exit code 0.")
